c7c851cfadee15dd308d9e3559f9916b7369e16b
belen-sosaa <belensoto941@gmail.com>
2026-05-05 19:58:52 -0400

    Actualización

--- src/pages/Lugares.jsx
+++ src/pages/Lugares.jsx
@@ -1,21 +1,29 @@
 import ValleLasTrancas from "../assets/images/Nevados-de-Chillan-1.jpg";
+import TermasDeChillan from "../assets/images/valle-hermoso-01.jpg";
+import LagunaLaja from "../assets/images/4384_Laguna_del_Laja.jpg";
+import CascadaTurbinas from "../assets/images/turbina01.jpg"
 
 function Lugares() {
   const lugares = [
     {
       nombre: "Valle Las Trancas",
-      descripcion: "Hermoso destino de montaña ubicado en la cordillera de los Andes, ideal para descansar y desconectarse del ritmo de la ciudad. Se encuentra rodeado de imponentes paisajes naturales, bosques nativos, ríos de origen volcánico y montañas que lo convierten en un entorno privilegiado durante todo el año.",
+      descripcion: "Es un hermoso destino de montaña ubicado en la cordillera de los Andes, ideal para descansar y desconectarse del ritmo de la ciudad. Se encuentra rodeado de imponentes paisajes naturales, bosques nativos, ríos de origen volcánico y montañas, lo que lo convierte en un entorno privilegiado durante todo el año.",
       imagen: ValleLasTrancas
     },
     {
-      nombre: "Termas de Chillán",
-      descripcion: "Relajo total en aguas termales",
-      imagen: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e"
+      nombre: "Nevados de Chillán",
+      descripcion: "Destino de montaña y centro termal ubicado en plena cordillera de los Andes, rodeado de bosques nativos, volcanes y paisajes naturales de gran belleza. Destaca por sus aguas termales de origen volcánico, ideales para el descanso, la relajación y el bienestar en un entorno natural único. Es un lugar turístico muy visitado durante todo el año, ya que combina naturaleza, tranquilidad y actividades al aire libre como senderismo y deportes de nieve en invierno.",
+      imagen: TermasDeChillan
     },
     {
-      nombre: "Plaza de Armas de Chillán",
-      descripcion: "Centro histórico y cultural",
-      imagen: "https://images.unsplash.com/photo-1491553895911-0055eca6402d"
+      nombre: "Parque Nacional Laguna del Laja",
+      descripcion: "Parque natural dominado por el volcán Antuco y la Laguna del Laja, ideal para senderismo y observación de paisajes volcánicos.",
+      imagen: LagunaLaja
+    },
+    {
+      nombre: "Cascada de las Turbinas",
+      descripcion: "Caída de agua de gran belleza natural, muy visitada por su entorno tranquilo y accesible.",
+      imagen: CascadaTurbinas
     }
   ];
 
