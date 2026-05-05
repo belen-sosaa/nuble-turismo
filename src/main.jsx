365b3c91ce35c3b987900319af1131a634750df0
belen-sosaa <belensoto941@gmail.com>
2026-05-05 19:04:46 -0400

    Mejora

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import './styles.css'
-import "leaflet/dist/leaflet.css"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import "./index.css";
+import "./styles.css";
+import "leaflet/dist/leaflet.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
