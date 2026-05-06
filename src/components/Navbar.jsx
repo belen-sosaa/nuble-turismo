4bb249bef70519a5d3e5bc11f6471757c5a6cd9c
belen-sosaa <belensoto941@gmail.com>
2026-05-05 20:30:38 -0400

    Mejora

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,18 @@
-import logo from "../assets/images/ChatGPT Image 5 may 2026, 08_03_14 p.m..png";
+import { Link } from "react-router-dom";
 
-function Navbar({ setPagina }) {
+function Navbar() {
   return (
     <nav className="navbar-pro">
-
-      <div className="navbar-left" style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-        <img
-          src={logo}
-          alt="Logo Ñuble Turismo"
-          style={{ width: "40px", height: "40px", objectFit: "contain" }}
-        />
+      <div className="navbar-left">
         <h2>Ñuble Turismo</h2>
       </div>
 
       <div className="navbar-center">
-        <button onClick={() => setPagina("home")}>Inicio</button>
-        <button onClick={() => setPagina("lugares")}>Lugares</button>
-        <button onClick={() => setPagina("desafios")}>Desafíos</button>
-        <button onClick={() => setPagina("mapa")}>Mapa</button>
+        <Link to="/">Inicio</Link>
+        <Link to="/lugares">Lugares</Link>
+        <Link to="/desafios">Desafíos</Link>
+        <Link to="/mapa">Mapa</Link>
       </div>
-
     </nav>
   );
 }
